cf38653c5e933cd81a0ad920fd00fec15e15d29b
anita-z <anita.zhang@piccogroup.com>
2024-11-12 15:40:58 -0500

    Need to fix: same port issue

--- hubs-browser/config.js
+++ hubs-browser/config.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 
-let { APS_CLIENT_ID, APS_CLIENT_SECRET, APS_CALLBACK_URL, SERVER_SESSION_SECRET, PORT } = process.env;
-if (!APS_CLIENT_ID || !APS_CLIENT_SECRET || !APS_CALLBACK_URL || !SERVER_SESSION_SECRET) {
-    console.warn('Missing some of the environment variables.');
+let { APS_CLIENT_ID, APS_CLIENT_SECRET, SERVER_SESSION_SECRET, PORT } = process.env;
+
+if (!APS_CLIENT_ID || !APS_CLIENT_SECRET || !SERVER_SESSION_SECRET) {
+    console.warn('Missing some of the required environment variables.');
     process.exit(1);
 }
+
+// Set a default port if none is specified in the .env file
 PORT = PORT || 8080;
-APS_CALLBACK_URL=`http://localhost:${PORT}/api/auth/callback` // URL your users will be redirected to after logging in with their Autodesk account
+
+
+// Dynamically set the callback URL based on the current PORT
+const APS_CALLBACK_URL = `http://localhost:${PORT}/api/auth/callback`;
 
 module.exports = {
     APS_CLIENT_ID,
