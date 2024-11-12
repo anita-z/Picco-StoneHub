6f95300d57cba5d903a3d60524e3cd39c658f0c1
anita-z <anita.zhang@piccogroup.com>
2024-11-12 13:27:02 -0500

    Fix callback url issue

--- hubs-browser/config.js
+++ hubs-browser/config.js
@@ -6,6 +6,7 @@ if (!APS_CLIENT_ID || !APS_CLIENT_SECRET || !APS_CALLBACK_URL || !SERVER_SESSION
     process.exit(1);
 }
 PORT = PORT || 8080;
+APS_CALLBACK_URL=`http://localhost:${PORT}/api/auth/callback` // URL your users will be redirected to after logging in with their Autodesk account
 
 module.exports = {
     APS_CLIENT_ID,
