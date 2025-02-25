d2e9b839e0d9e3fa0bd89d8e81a87dcc8514b0fd
anita-z <anita.zhang@piccogroup.com>
2025-02-25 09:05:52 -0500

    Savepoint for adpating to firebase functions

--- stone-hub/functions/index.js
+++ stone-hub/functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,3 +17,23 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+const functions = require("firebase-functions");
+
+const app = require("../server")
+exports.app = functions.https.onRequest(app);
+
+// const express = require("express");
+// const app = express();
+
+// // Import your routers
+// const authRouter = require("../routes/auth");
+// const hubsRouter = require("../routes/hubs");
+
+// // Mount the routers
+// app.use(authRouter);
+// app.use(hubsRouter);
+
+// // Export the Express app as "api"
+// exports.api = functions.https.onRequest(app);
