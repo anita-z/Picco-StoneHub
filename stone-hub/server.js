e1b0a1d47fa789b7f59e5ecf202f67c503334249
anita-z <anita.zhang@piccogroup.com>
2025-02-25 14:26:21 -0500

    Savepoint for connecting to firestore

--- stone-hub/server.js
+++ stone-hub/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('cookie-session');
 const { SERVER_SESSION_SECRET } = require('./config.js');
-require('dotenv').config();
+const serviceAccount = require("./serviceAccountKey.json");
+// import { collection, addDoc } from "firebase/firestore"; 
 
 let app = express();
 app.use(express.static('wwwroot'));
@@ -9,10 +11,40 @@ app.use(session({ secret: SERVER_SESSION_SECRET, maxAge: 24 * 60 * 60 * 1000 }))
 app.use(require('./routes/auth.js'));
 app.use(require('./routes/hubs.js'));
 
+// const { initializeApp, cert } = require("firebase-admin/app");
+// const { getFirestore } = require("firebase-admin/firestore");
+// const { collection, addDoc } = require("firebase/firestore");
+
+// initializeApp({
+//   credential: cert(serviceAccount),
+//   databaseURL: process.env.databaseURL
+// });
+
+// const db = getFirestore();
+
+// app.post("/testDB", (req, res) => {
+
+//   let setDoc =
+//     db.collection('testCollection').doc('testDoc').set(req.body);
+
+//   res.send({ 'Message': 'Success' });
+
+// });
+
+// try {
+//   const docRef = await addDoc(collection(db, "users"), {
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815
+//   });
+//   console.log("Document written with ID: ", docRef.id);
+// } catch (e) {
+//   console.error("Error adding document: ", e);
+// }
 
 // Only listen on a port if we’re NOT running in Cloud Functions
 if (process.env.FUNCTIONS_EMULATOR) {
-    // If you’re not using the Firebase emulator, run the local server
-    const PORT = 8080;
-    app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
-  }
+  // If you’re not using the Firebase emulator, run the local server
+  const PORT = 8080;
+  app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
+}
