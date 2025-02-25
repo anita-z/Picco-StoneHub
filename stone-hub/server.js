539a71d14be398a339c34814f0b3ab78d8d16192
anita-z <anita.zhang@piccogroup.com>
2025-02-25 16:10:45 -0500

    Connect to firestore

--- stone-hub/server.js
+++ stone-hub/server.js
@@ -2,45 +2,43 @@ require('dotenv').config();
 const express = require('express');
 const session = require('cookie-session');
 const { SERVER_SESSION_SECRET } = require('./config.js');
-const serviceAccount = require("./serviceAccountKey.json");
-// import { collection, addDoc } from "firebase/firestore"; 
 
+// Initialize express app
 let app = express();
 app.use(express.static('wwwroot'));
 app.use(session({ secret: SERVER_SESSION_SECRET, maxAge: 24 * 60 * 60 * 1000 }));
 app.use(require('./routes/auth.js'));
 app.use(require('./routes/hubs.js'));
 
-// const { initializeApp, cert } = require("firebase-admin/app");
-// const { getFirestore } = require("firebase-admin/firestore");
-// const { collection, addDoc } = require("firebase/firestore");
+// Initialize Firebase admin SDK
+// Reference: 
+//  How to create and connect to a firestore database in an express app ?
+//  https://dev.to/ikramkharbouch/how-to-create-and-connect-to-a-firestore-database-in-an-express-app--4j84
 
-// initializeApp({
-//   credential: cert(serviceAccount),
-//   databaseURL: process.env.databaseURL
-// });
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
-// const db = getFirestore();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.databaseURL
+});
 
-// app.post("/testDB", (req, res) => {
+const db = admin.firestore();
 
-//   let setDoc =
-//     db.collection('testCollection').doc('testDoc').set(req.body);
+// test on firestore connection, success :)
+// everytime the below codes run, it will create a new document inside "testCollection"
+/*
+app.get('/test-firestore', async (req, res) => {
+  try {
+    const docRef = await db.collection('testCollection').add({ message: 'Hello Firestore!' });
+    return res.send(`Created doc with ID: ${docRef.id}`);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Error writing to Firestore');
+  }
+});
+*/
 
-//   res.send({ 'Message': 'Success' });
-
-// });
-
-// try {
-//   const docRef = await addDoc(collection(db, "users"), {
-//     first: "Ada",
-//     last: "Lovelace",
-//     born: 1815
-//   });
-//   console.log("Document written with ID: ", docRef.id);
-// } catch (e) {
-//   console.error("Error adding document: ", e);
-// }
 
 // Only listen on a port if we’re NOT running in Cloud Functions
 if (process.env.FUNCTIONS_EMULATOR) {
