da10abeb143e7c5fa9c64251f704bd4268dbf4f9
anita-z <anita.zhang@piccogroup.com>
2025-02-27 18:52:55 -0500

    Encode firebase admin credential & add it to env var

--- stone-hub/db.js
+++ stone-hub/db.js
@@ -4,7 +4,12 @@
 //  https://dev.to/ikramkharbouch/how-to-create-and-connect-to-a-firestore-database-in-an-express-app--4j84
 
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+// const serviceAccount = require("./serviceAccountKey.json");
+
+// Decode the Base64 string into a JSON object
+const { FIREBASE_CREDENTIALS } = require('./config.js');
+const serviceAccount = JSON.parse(Buffer.from(FIREBASE_CREDENTIALS, "base64").toString("utf-8"));
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
