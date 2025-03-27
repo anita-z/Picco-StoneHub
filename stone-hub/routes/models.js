3bad0861ada6f7abf8c8d6d78bde4ace517b58db
anita-z <anita.zhang@piccogroup.com>
2025-03-27 00:52:05 -0400

    Add individual model fetch route; Add error handling for non-existing data

--- stone-hub/routes/models.js
+++ stone-hub/routes/models.js
@@ -10,6 +10,11 @@ let router = express.Router();
 router.get('/firebase/models', async (req, res) => {
     try {
         const models = await db.collection('models').get();
+
+        if (!models.exists) {
+            return res.status(404).json({ error: 'Model not found' });
+        }
+
         const modelDocs = models.docs.map(doc => ({ id: doc.id, ...doc.data() }));
         res.json(modelDocs);
     } catch (err) {
@@ -18,9 +23,29 @@ router.get('/firebase/models', async (req, res) => {
     }
 });
 
+router.get('/firebase/models/:model_urn', async (req, res) => {
+    try {
+        const modelDoc = await db.collection('models').doc(req.params.model_urn).get();
+
+        if (!modelDoc.exists) {
+            return res.status(404).json({ error: 'Model not found' });
+        }
+
+        res.json(modelDoc);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Error reading Firestore "model" document');
+    }
+});
+
 router.get('/firebase/models/:model_urn/elements', async (req, res) => {
     try {
         const elements = await db.collection('models').doc(req.params.model_urn).collection('stone_elements').get();
+
+        if (!elements.exists) {
+            return res.status(404).json({ error: 'Model not found' });
+        }
+
         const elementDocs = elements.docs.map(doc => ({ id: doc.id, ...doc.data() }));
         res.json(elementDocs);
     } catch (err) {
@@ -32,6 +57,11 @@ router.get('/firebase/models/:model_urn/elements', async (req, res) => {
 router.get('/firebase/models/:model_urn/elements/:element_id', async (req, res) => {
     try {
         const elementsDoc = await db.collection('models').doc(req.params.model_urn).collection('stone_elements').doc(req.params.element_id).get();
+
+        if (!elementsDoc.exists) {
+            return res.status(404).json({ error: 'Model not found' });
+        }
+
         res.json(elementsDoc.data());
     } catch (err) {
         console.error(err);
@@ -61,16 +91,16 @@ router.post('/firebase/update/stones/table/data', async (req, res) => {
     }
 });
 
-router.post('/firebase/update/stones/table/config', async (req, res) => {
+router.post('/firebase/update/stones/table/column_definitions', async (req, res) => {
 
     console.log("Received config update request:", req.body);
 
-    const { model_urn, value, table_config_type } = req.body;
+    const { model_urn, value, table_type } = req.body;
 
     try {
         await db.collection('models')
             .doc(model_urn)
-            .update({ [table_config_type]: FieldValue.arrayUnion(value) });
+            .update({ [table_type]: FieldValue.arrayUnion(value) });
 
         res.status(200).send({ success: true });
     } catch (err) {
